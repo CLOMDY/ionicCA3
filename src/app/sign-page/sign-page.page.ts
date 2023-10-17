d83ea4ca40823c5d3ca5a0018a1e83d81492382d
Vimarsh Pandey <vimarsh240501@gmail.com>
2023-10-17 15:57:01 +0530

    Add files via upload

--- src/app/sign-page/sign-page.page.ts
+++ src/app/sign-page/sign-page.page.ts
@@ -5,9 +5,21 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './sign-page.page.html',
   styleUrls: ['./sign-page.page.scss'],
 })
-export class SignPagePage implements OnInit {
+export class SignPagePage implements OnInit
+{
+  signin_firstname:any;
+  signin_lastname:any;
+  signin_email:any;
+  signin_phone:any;
+  signin_password:any;
+  signin_confirmpassword:any;
 
   constructor() { }
+
+  signinForm(e:any)
+  {
+    console.log("crequested Data: ",e.value);
+  }
 
   ngOnInit() {
   }
